287f444ef245c918a089aba5ef4309f68aead2ce
contact.abeyene@gmail.com <contact.abeyene@gmail.com>
2018-11-20 21:04:52 +0000

    Added SSR server folder

---
+++ server/bootstrap.js
@@ -0,0 +1,10 @@
+process.env.NODE_ENV = 'development'
+
+require('ignore-styles');
+
+require('@babel/register')({
+    ignore: [ /(node_modules)/ ],
+    presets: ['@babel/preset-react', '@babel/preset-env']
+});
+
+require('./index');
